30c75f3799c264131b14a121ecf0a9bcfc7460ca
Rafaf Tahsin <rafaf.tahsin@infolytx.com>
2024-04-21 10:29:57 +0600

    added 36

---
+++ 36/36.h
@@ -0,0 +1,19 @@
+//
+// Created by rafaf.tahsin on 4/21/24.
+//
+
+#ifndef LEETCODE_36_H
+#define LEETCODE_36_H
+
+#endif //LEETCODE_36_H
+
+using namespace std;
+
+namespace leetcode36 {
+    class Solution {
+    public:
+        bool isValidSudoku(vector<vector<char>>& board) {
+            return false;
+        }
+    };
+}
