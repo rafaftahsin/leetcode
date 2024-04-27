daa4dae4328d9fda66d8cfcc8e60781c0887345f
Rafaf Tahsin <rafaf.tahsin@infolytx.com>
2024-04-27 11:56:46 +0600

    solved 739

--- 739/739.h
+++ 739/739.h
@@ -2,6 +2,8 @@
 // Created by rafaf.tahsin on 4/27/24.
 //
 
+#include <queue>
+
 #ifndef LEETCODE_739_H
 #define LEETCODE_739_H
 
@@ -11,17 +13,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> v;
+        auto cmp = [&temperatures](int &a, int &b) -> bool { return temperatures[a] > temperatures[b]; };
+        priority_queue< int, vector<int>, decltype(cmp) > pq(cmp);
 
-        for( vector<int>::iterator it=temperatures.begin(); it!=temperatures.end(); it++) {
-            vector<int>::iterator found_at = find_if(it, temperatures.end(), [it]( int elem) -> bool { return *it < elem; });
-
-            if(found_at == temperatures.end())
-                v.push_back(0);
-            else
-                v.push_back(distance(it, found_at));
+        vector<int> result(temperatures.size(), 0);
+        for( int i=0; i<temperatures.size(); i++) {
+            while ( !pq.empty() && temperatures[pq.top()] < temperatures[i]) {
+                result[pq.top()] = i - pq.top();
+                pq.pop();
+            }
+            pq.push(i);
         }
 
-        return v;
+        return result;
     }
 };
