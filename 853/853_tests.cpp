c8c5cc4c795c364af48cc0f46f403b40fd0fa19c
Rafaf Tahsin <rafaf.tahsin@infolytx.com>
2024-05-07 16:45:38 +0600

    update

--- 853/853_tests.cpp
+++ 853/853_tests.cpp
@@ -20,4 +20,28 @@ BOOST_AUTO_TEST_SUITE(test_suite)
 
         BOOST_TEST( result == expected );
     }
+
+    BOOST_AUTO_TEST_CASE(test_case_2)
+    {
+        int target = 10;
+        vector <int > position {8,3,7,4,6,5};
+        vector <int > speed { 4,4,4,4,4,4 };
+
+        int result = sol.carFleet(target, position, speed);
+        int expected = 6;
+
+        BOOST_TEST( result == expected );
+    }
+
+    BOOST_AUTO_TEST_CASE(test_case_3)
+    {
+        int target = 100;
+        vector <int > position { 0,2,4 };
+        vector <int > speed { 4,2,1 };
+
+        int result = sol.carFleet(target, position, speed);
+        int expected = 1;
+
+        BOOST_TEST( result == expected );
+    }
 BOOST_AUTO_TEST_SUITE_END()
---
+++ 853/readme.md
@@ -0,0 +1,19 @@
+### Case 3 
+
+In the statement of the question, It didn't provide further instructions after several cars become a fleet.
+
+
+```text
+10
+[0,4,2]
+[2,1,3]
+```
+
+
+1st run: 2 5 5 (1st and 2nd became a fleet, I assume the answer should be TWO since there are no further instructions on the question or the example)
+However, after a couple of runs:
+2nd run: 4,6,8
+3rd run: 6,7,11
+4th run: 8,8,14 (0th and 1st became a fleet, the answer is ONE)
+
+Based on the result of the test, I assume that the until all cars arrive at or pass the destination, there are some potential cases that any car could join to a fleet. It's better to state or clarify this part.
